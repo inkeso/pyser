213ee4c64ba1cab6e964fa3259a0a7502b3c18ab
inkeso <inkeso@gmx.de>
2019-11-05 22:43:46 +0100

    moved togglestates to Gui()-Class

--- pyser.py
+++ pyser.py
@@ -17,6 +17,7 @@
 import curses
 import serial
 import widgets
+import translate
 # https://docs.python.org/3/howto/curses.html
 # https://docs.python.org/3/library/curses.html
 
@@ -30,6 +31,51 @@ SNDDUMP = "/dev/null" # record Sent bytes to a file
 lips = "Lörem ipsüm → dolor sit amet, consectetur 😉 adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enimad minim veniam, quis nostrud exercitation ullamco laboris."
 
 
+class Toggle():
+    def __init__(self, win, y, x, key, choices, hint=None, current=0):
+        self.win = win
+        self.y, self.x = y, x
+        self.key = key
+        self.hint = hint
+        self.choices = choices
+        self.current = current
+        self.smaxlen = max(map(len, self.choices))
+        self.display()
+        
+    def nextState(self):
+        self.current = (self.current + 1) % len(self.choices)
+        self.display()
+        
+    def maxLen(self):
+        return max(map(len, s))
+        
+    def display(self):
+        cy, cx = self.win.getyx()
+        self.win.addstr(self.y, self.x, " %s " % self.key, widgets.COLOR["key"])
+        if self.hint: self.win.addstr("(%s) " % self.hint, widgets.COLOR["hint"])
+        self.win.addstr(self.getState().center(self.smaxlen)+" ", widgets.COLOR["state"])
+        self.win.move(cy, cx)
+        self.win.refresh()
+    
+    def getState(self):
+        return self.choices[self.current]
+
+class Key():
+    def __init__(self, win, y, x, key, text, action):
+        self.win = win
+        self.y, self.x = y, x
+        self.key = key
+        self.text = text
+        self.action = action
+        self.display()
+
+    def display(self):
+        cy, cx = self.win.getyx()
+        self.win.addstr(self.y, self.x, " %s " % self.key, widgets.COLOR["key"])
+        self.win.addstr(self.text+" ", widgets.COLOR["state"])
+        self.win.move(cy, cx)
+        self.win.refresh()
+
 class Gui():
     HELP = """
         F1 : Show this help
@@ -40,32 +86,53 @@ class Gui():
         F7 : Toggle Translation (in textview)
         F8 : Toggle display of input / output in hexdump
         F10: Quit
-        
+
         PgUp/PgDn: Scroll ⅓ Page up/down
         Home/End : Scroll to top or bottom
-        
+
         ↓/↑ : go through input-history (repeat command)
     \n"""
-    
+
     def __init__(self):
         ay, ax = curses.LINES, curses.COLS
         self.out_asc = widgets.TxtWin("", ay-3, ax//2, 0, 0)
-        self.out_asc.showTranslate()
         self.out_hex = widgets.TxtWin("Hexdump", ay-3, ax//2, 0, ax//2)
-        self.out_hex.showHex()
         self.in_str = widgets.Input(3, ax, ay-3, 0)
+        self.n2brk = {"None":"", "LF":"\n", "CR": "\r", "CRLF": "\r\n", "0x00": "\0"}
+
+        in_w = self.in_str.coords[1]
+        hx_s = self.out_hex.coords
+        self.keys = {
+            curses.KEY_F1:  Key(self.in_str.win,  2,         in_w-24,    "F1",  "Help",         lambda: self.message(self.HELP)),
+            curses.KEY_F4:  Key(self.out_hex.win, hx_s[0]-1, hx_s[1]-20, "F4",  "Clear Output", self.emptyView),
+            curses.KEY_F10: Key(self.in_str.win,  2,         in_w-13,    "F10", "Quit",         self.quit)
+        }
+        
+        as_s = self.out_asc.coords[1]
+        self.tInp = Toggle(self.in_str.win,  2, 3,       "F5", ["ASCII", "Hex", "File"],      "Mode")
+        self.tBrk = Toggle(self.in_str.win,  2, 22,      "F6", list(self.n2brk.keys()),       "Append")
+        self.tAsc = Toggle(self.out_asc.win, 0, as_s-31, "F7", list(translate.PAGES.keys()),  "Codepage")
+        self.tHex = Toggle(self.out_hex.win, 0, 3,       "F8", ["Both", "Receive", "Send"],   "Show Stream")
+
+        self.toggles = {
+            curses.KEY_F5: self.tInp,
+            curses.KEY_F6: self.tBrk,
+            curses.KEY_F7: self.tAsc,
+            curses.KEY_F8: self.tHex,
+        }
+        
+        self.running = True
 
     def show(self, s, color): # show stuff in both views and scroll to bottom
-        self.out_asc.append(s, color)
-        if (self.out_hex.inHex == 0) \
-            or (self.out_hex.inHex == 1 and color=="text") \
-            or (self.out_hex.inHex == 2 and color=="send"):
+        # translate input for ascii. always show everything there
+        self.out_asc.append(translate.translate(s, self.tAsc.getState()), color)
+        
+        # selectively untouched hexdump
+        th = self.tHex.getState()
+        if th == "Both" or th+color in ("Receivetext", "Sendsend"):
             self.out_hex.appendHex(s, color)
-        # toggle off? pause?
-        self.out_hex.scroll("end")
-        self.out_asc.scroll("end") 
-        
-        
+        self.bscroll("end")
+
     def message(self, s): # show message with different color only in textview
         self.out_asc.append(s, "offset")
         self.out_asc.scroll("end")
@@ -82,14 +149,16 @@ class Gui():
         self.out_asc.pad.erase()
         self.out_hex.pad.erase()
         self.out_hex.hexoffset = 0
-        self.out_asc.scroll("end")
-        self.out_hex.scroll("end")
+        self.bscroll("end")
+        
+    def quit(self):
+        self.running = False
 
 def main(scr):
     scr.clear()
     scr.nodelay(True)
     scr.getch()
-    
+
     # Styles (see below)
     curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLUE)  # header
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)  # Received data
@@ -97,7 +166,7 @@ def main(scr):
     curses.init_pair(4, curses.COLOR_RED, curses.COLOR_BLACK)    # ERROR
     curses.init_pair(5, curses.COLOR_YELLOW, curses.COLOR_BLACK) # offset
     curses.init_pair(6, curses.COLOR_WHITE, curses.COLOR_BLUE)   # status
-    
+
     widgets.COLOR = {
         "header" : curses.color_pair(1) + curses.A_BOLD,
         "text"   : curses.color_pair(2),
@@ -108,7 +177,7 @@ def main(scr):
         "hint"   : curses.color_pair(6) + curses.A_ITALIC,
         "state"  : curses.color_pair(6)
     }
-    
+
     gui = Gui()
 
     # Prepare Serial Port
@@ -120,52 +189,46 @@ def main(scr):
         ser = None
         gui.error("CONNECTION FAILED\n\n")
         gui.message(str(e))
-    
-    while True:
+
+    while gui.running:
         gui.in_str.win.refresh()
         c = scr.getch()
 
-        # TODO: Inputhandling kann so nich bleiben. aber erstmal langts.
-        # Also je nach Modus wäre ja GNU readline geil...
-        if c == -1: # no input
+        # TODO: Better input handling (←/→ navigate/insert), overflow
+        if c == -1: # no input: poll/read serial
             if ser:
                 rx = ser.read()
                 while ser.inWaiting() > 0: rx += ser.read(ser.inWaiting())
-                if len(rx) > 0: 
+                if len(rx) > 0:
                     data = "".join(chr(x) for x in rx)
                     gui.show(data, "text")
-        
+
         elif 31 < c < 127: # ASCII
             gui.in_str.append(chr(c))
-        
+
         elif c == 10: # ENTER
-            gui.show(gui.in_str.getInput(), "send")
-            if ser: ser.write(gui.in_str.getBytes())
+            end = gui.n2brk[gui.tBrk.getState()]
+            gui.show(gui.in_str.getInput(end), "send")
+            if ser: ser.write(gui.in_str.getBytes(end))
             gui.in_str.clear()
-    
+
         elif c == curses.KEY_BACKSPACE: gui.in_str.backspace()
         elif c == curses.KEY_UP: gui.in_str.goHistory(1)
         elif c == curses.KEY_DOWN: gui.in_str.goHistory(-1)
-
-        elif c == curses.KEY_F1: gui.message(gui.HELP)
-        elif c == curses.KEY_F2: pass
-        elif c == curses.KEY_F4: gui.emptyView()
-        elif c == curses.KEY_F5: gui.in_str.nextState()
-        elif c == curses.KEY_F6: gui.in_str.nextBreak()
-        elif c == curses.KEY_F7: gui.out_asc.nextTranslate()
-        elif c == curses.KEY_F8: gui.out_hex.nextHex()
-        elif c == curses.KEY_F10: return
-
+        
         elif c == curses.KEY_NPAGE: gui.bscroll("down")
         elif c == curses.KEY_PPAGE: gui.bscroll("up")
         elif c == curses.KEY_HOME: gui.bscroll("home")
         elif c == curses.KEY_END: gui.bscroll("end")
+
+        elif c in gui.keys: gui.keys[c].action()
+        elif c in gui.toggles: gui.toggles[c].nextState()
 
         elif c == curses.KEY_F11: gui.show(lips, "send") # For fun an testing
 
         #else:
         #    out_asc.append("Ignored Key: %d\n" % c , "offset")
         #    out_asc.display()
-        
+
 curses.wrapper(main)
 
--- widgets.py
+++ widgets.py
@@ -6,72 +6,42 @@ import translate
 
 COLOR = {"header" : 0, "text" : 0, "send" : 0, "error": 0, "offset" : 0, "key":0, "hint":0, "state": 0}
 
-class TxtWin(): 
+class TxtWin():
     """Simple widget for displaying text and allowing for scroll"""
     # You can speed performance and perhaps reduce screen flicker by issuing
     # noutrefresh() calls on all windows, followed by a single curses.doupdate().
-    
+
     PADSIZE = 2000
-    hexModes = ["Both", "Receive", "Send"]
-    
+
     def __init__(self, title, h, w, y, x):
-        self.coords = (h,w,y,x) 
+        self.coords = (h,w,y,x)
         self.win = curses.newwin(h,w,y,x)
         self.win.border()
         if title: self.win.addstr(0, 3, " %s " % title, COLOR["header"])
         self.win.refresh()
         self.scrollpos = 0
         self.hexoffset = 0
-        self.inHex = 0
-        self.inTranslate = 0
-        self.trTabs = list(translate.PAGES.keys())
         self.pad = curses.newpad(self.PADSIZE, w-2)
         self.pad.move(0,0)
-    
+
     def display(self):
         h,w,y,x = self.coords
         self.pad.refresh(self.scrollpos, 0, y+1, x+1, y+h-2, x+w-2)
-    
+
     def scroll(self, pos="down"):
         lpos = self.pad.getyx()[0] - self.coords[0] + 3
         scrollins = self.coords[0] // 3 # ⅓ Screen
         if pos == "down": self.scrollpos += scrollins
         if pos == "up": self.scrollpos -= scrollins
         if pos == "home": self.scrollpos = 0
-        if pos == "end": self.scrollpos = lpos 
-        
+        if pos == "end": self.scrollpos = lpos
+
         if self.scrollpos > lpos: self.scrollpos = lpos
         if self.scrollpos < 0: self.scrollpos = 0
-        
+
         self.display()
-    
-    def nextTranslate(self):
-        self.inTranslate = (self.inTranslate + 1) % len(self.trTabs)
-        self.showTranslate()
-        
-    def showTranslate(self):
-        self.win.addstr(0, self.coords[1] - 43, " F4 ", COLOR["key"])
-        self.win.addstr("Clear ", COLOR["state"])
 
-        self.win.addstr(0, self.coords[1] - 31, " F7 ", COLOR["key"])
-        self.win.addstr("(Codepage) ", COLOR["hint"])
-        self.win.addstr("%12s " % self.trTabs[self.inTranslate], COLOR["state"])
-        self.win.refresh()
-
-    def nextHex(self):
-        self.inHex = (self.inHex + 1) % len(self.hexModes)
-        self.showHex()
-    
-    def showHex(self):
-        self.win.addstr(0, 3, " F8 ", COLOR["key"])
-        self.win.addstr("(Show Stream) ", COLOR["hint"])
-        self.win.addstr("%6s " % self.hexModes[self.inHex], COLOR["state"])
-        self.win.refresh()
-        
     def append(self, s, color="text"):
-        # First of all: sanitize input
-        s = translate.translate(s, self.trTabs[self.inTranslate])
-        
         # check if pad has still enough space left.
         br = self.coords[1]-2
         nrows = sum([len(x) // br + 1 for x in s.split("\n")])
@@ -85,14 +55,14 @@ class TxtWin():
                 cy = 0
                 cx = 0
             self.pad.move(cy, cx)
-        
+
         try: # this may still explode if something bigger than buffer is comming in (unlikely)
             self.pad.addstr(s, COLOR[color])
         except curses.error:
             cy, cx = self.pad.getyx()
             self.pad.move(cy, 0)
             self.pad.addstr(" *** TRUNCATED *** ", COLOR["error"])
-        
+
     def appendHex(self, s, color="text"):
         for c in s.encode():
             if self.hexoffset % 16 == 0:
@@ -102,31 +72,25 @@ class TxtWin():
             self.pad.move(cy, 10 + roff * 3)
             self.pad.addstr("%02X" % c, COLOR[color])
             self.pad.move(cy, 60 + roff)
-            # vielleicht, wenn translate "CP...", kann man dann doch...
             self.pad.addstr(chr(c) if 31 < c < 127 else "·", COLOR[color])
             self.hexoffset += 1
 
 class Input():
     """Our simple Input-Widget"""
 
-    stateStr = ["ASCII"] #, "Hex", "File"]
-    breakStr = [["","None"], ["\n"," LF "], ["\r"," CR "], ["\r\n", "CRLF"], ["\0", "0x00"]]
-
     def __init__(self, h, w, y, x):
         self.coords = (h, w, y, x)
         self.win = curses.newwin(h, w, y, x)
+        self.win.border()
         self.inp = ""
-        self.inState = 0
-        self.inBreak = 0
         self.history = []
         self.shist = -1
         self.redraw()
-        
+
     def redraw(self):
-        self.win.erase()
-        self.win.border()
+        self.win.addstr(1,1, " "*(self.coords[1]-2))
         self.win.addstr(1,1, self.inp)
-        self.showState()
+        self.win.refresh()
 
     def clear(self):
         if self.inp and (len(self.history) == 0 or self.inp != self.history[0]):
@@ -134,7 +98,7 @@ class Input():
         self.inp = ""
         self.shist = -1
         self.redraw()
-    
+
     def append(self, c):
         try:
             self.inp += c
@@ -162,41 +126,11 @@ class Input():
         if cx < 1: cx = 1
         self.win.addstr(cy, cx," ")
         self.win.move(cy, cx)
-    
-    def nextBreak(self):
-        self.inBreak = (self.inBreak + 1) % len(self.breakStr)
-        self.showState()
 
-    def nextState(self):
-        self.inState = (self.inState + 1) % len(self.stateStr)
-        self.showState()
-    
-    def showState(self):
-        cy, cx = self.win.getyx()
-        w = self.coords[1]
-        self.win.addstr(2, 3, " F5 ", COLOR["key"])
-        self.win.addstr("(Mode) ", COLOR["hint"])
-        self.win.addstr("%5s " % self.stateStr[self.inState], COLOR["state"])
-        self.win.addstr(2, 22, " F6 ", COLOR["key"])
-        self.win.addstr("(Append) ", COLOR["hint"])
-        self.win.addstr("%4s " % self.breakStr[self.inBreak][1], COLOR["state"])
-        
-        self.win.addstr(2, 22, " F6 ", COLOR["key"])
-        self.win.addstr("(Append) ", COLOR["hint"])
-        self.win.addstr("%4s " % self.breakStr[self.inBreak][1], COLOR["state"])
-        
-        self.win.addstr(2, w-24, " F1 ", COLOR["key"])
-        self.win.addstr("Help ", COLOR["state"])
-        self.win.addstr(2, w-13, " F10 ", COLOR["key"])
-        self.win.addstr("Quit ", COLOR["state"])
-        
-        self.win.move(cy, cx)
-        self.win.refresh()
-    
-    def getInput(self):
-        return self.inp + self.breakStr[self.inBreak][0]
-    
-    def getBytes(self):
+    def getInput(self, end=""):
+        return self.inp + end
+
+    def getBytes(self, end=""):
         # obacht bei höherwertigem UTF-8!
-        return bytes([ord(x) for x in self.getInput()])
-        
+        return bytes([ord(x) for x in self.getInput() + end])
+
